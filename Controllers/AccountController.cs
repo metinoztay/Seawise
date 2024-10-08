287bc68ab071793b81bada9f5f2d66e6946c5e63
metinoztay <metinoztay@gmail.com>
2024-10-08 22:23:38 +0300

    Theme Data Added

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Seawise.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
